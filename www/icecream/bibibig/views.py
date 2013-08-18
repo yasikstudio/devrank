c619619ea353db89eb183cd0fa916443729e5e7e
Jweb <jweb@jweb.kr>
2013-08-18 15:02:11 +0900

    elasticsearch queryDSL modified. esclient added.

--- www/icecream/bibibig/views.py
+++ www/icecream/bibibig/views.py
@@ -4,56 +4,11 @@ from django.template import RequestContext, Context
 from django.shortcuts import *
 from django.views.generic.base import View
 import rawes
+from esutils.client import ESClient
 
 def home(request):
-    es = rawes.Elastic('http://jweb.kr:9200')
-
-    # es.post('github/_search',data=""" {
-    #               "query": {
-    #                 "term": {
-    #                   "login": "reeoss"
-    #                 }
-    #               }
-    #             } """)
-    # es.post('github/_search',data="""
-    #               {
-    #                   "query": {
-    #                     "filtered": {
-    #                       "query": { "term": { "language":"java"}},
-    #                       "filter": {
-    #                           "nested": {
-    #                           "path": "owner",
-    #                           "query": {
-    #                             "filtered": {
-    #                               "query": { "match_all": {}},
-    #                               "filter": {
-    #                               "or":[
-    #                               {"term": { "owner.login": "JSansalone" }},
-    #                               {"term": { "owner.login": "kunny" }}
-    #                               ]
-    #                               }
-    #                             }
-    #                           }
-    #                         }
-    #                       }
-    #                     }
-    #                   }
-    #                 } """)
-
-    search = 'repository'
-    query = '''
-    {
-        "query": {
-            "multi_match": {
-                "query": \"%s\" ,
-                "fields": [ "description", "language" ]
-            }
-        }
-    } ''' % (search)
-    result = es.post('github/_search',data=query)
-
-    print result['hits']['hits'][0]['_source']['language']
-
+    c = ESClient()
+    c.search('peanut')
 
     var = RequestContext(request, {
             'page_title': u'Home',
