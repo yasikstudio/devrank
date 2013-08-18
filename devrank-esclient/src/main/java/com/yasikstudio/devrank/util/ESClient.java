aabdd29782922b19e86519003fd8da029c29e510
Jongyeol Choi <jong10@gmail.com>
2013-08-18 16:39:26 +0900

    Add devrank-esclient

---
+++ devrank-esclient/src/main/java/com/yasikstudio/devrank/util/ESClient.java
@@ -0,0 +1,75 @@
+package com.yasikstudio.devrank.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class ESClient {
+  private static final String DATA =
+      "{\"script\": \"ctx._source.devrank_score = devrank_score\", \"params\": {\"devrank_score\": %.30f}}";
+  private static final String URL = "http://%s/github/users/%s/_update";
+  private String hostname;
+
+  public ESClient(String hostname) {
+    this.hostname = hostname;
+  }
+
+  public synchronized String update(String id, double devrank) {
+    String url = String.format(URL, hostname, id);
+    String data = String.format(DATA, devrank);
+    try {
+      return post(url, data);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private String post(String url, String data) throws ClientProtocolException,
+      IOException {
+    DefaultHttpClient httpclient = new DefaultHttpClient();
+    HttpPost httpPost = new HttpPost(url);
+    httpPost.setHeader("Accept", "application/json");
+    httpPost.setHeader("Content-Type", "application/json");
+    httpPost.setEntity(new StringEntity(data));
+    HttpResponse response = httpclient.execute(httpPost);
+
+    String contents = null;
+    try {
+      HttpEntity entity = response.getEntity();
+      contents = getContents(entity.getContent());
+      EntityUtils.consume(entity);
+    } finally {
+      httpPost.releaseConnection();
+    }
+    return contents;
+  }
+
+  private String getContents(InputStream is) {
+    StringBuilder sb = new StringBuilder();
+    if (is != null) {
+      try {
+        int len = 0;
+        byte[] buf = new byte[1024 * 1024];
+        while ((len = is.read(buf)) >= 0) {
+          sb.append(new String(buf, 0, len));
+        }
+      } catch (IOException e) {
+        return null;
+      } finally {
+        try {
+          is.close();
+        } catch (IOException e) {
+          // don't care
+        }
+      }
+    }
+    return sb.toString();
+  }
+}
---
+++ devrank-esclient/src/main/java/com/yasikstudio/devrank/util/ESLauncher.java
@@ -0,0 +1,42 @@
+package com.yasikstudio.devrank.util;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ESLauncher {
+  private static final String USAGE =
+      "Usage: java jar esclient-xxx-with-dependencies.jar [hostname] [filename]";
+
+  public static void main(String[] args) throws IOException {
+    if (args.length != 2) {
+      usageWithExit();
+    }
+    ESClient es = new ESClient(args[0]);
+    BufferedReader r =
+        new BufferedReader(new InputStreamReader(new FileInputStream(args[1])));
+    updateRanks(es, r);
+    r.close();
+  }
+
+  private static void updateRanks(ESClient es, BufferedReader r)
+      throws IOException {
+    String line = null;
+    while ((line = r.readLine()) != null) {
+      String[] data = line.split(",");
+      String uid = data[0];
+      double folValue = Double.parseDouble(data[1]);
+      double actValue = Double.parseDouble(data[2]);
+      double v = folValue + actValue;
+      System.out.print(String.format("%s : %.30f => ", uid, v));
+      String result = es.update(uid, v);
+      System.out.println(result);
+    }
+  }
+
+  private static void usageWithExit() {
+    System.out.println(USAGE);
+    System.exit(-1);
+  }
+}
