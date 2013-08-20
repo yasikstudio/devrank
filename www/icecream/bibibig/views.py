215592a8049deef6e26bc074042701b4c40838bb
rakgyu.song <reeoss@gmail.com>
2013-08-20 09:12:43 +0900

    apply CURD request

--- www/icecream/bibibig/views.py
+++ www/icecream/bibibig/views.py
@@ -7,35 +7,33 @@ from esutils.client import ESClient
 
 import json
 
-def home(request):
-    if request.method == 'POST':
-        c = ESClient()
-        r = c.search(request.POST['query'])
-
-        for _r in r:
-            _r.update({u"source":_r["_source"]})
-            if u"devrank_score" in _r[u"source"].keys():
-                value = float(_r[u"source"][u"devrank_score"]) * 1000000
-                _r["source"].update({u"devrank_score":value})
-            else:
-                _r["source"].update({u"devrank_score":0})
-            _r.pop(u"_source",None)
-
-        var = RequestContext(request, {
-                'page_title': u'Devrank',
-                'results': r,
-                'query':request.POST['query'],
-                })
-        return render_to_response('result_list.html', var)
-    else:
-        var = RequestContext(request, {'page_title': u'Home', })
+class home(View):
+    def get(self, request, *args, **kwargs):
+        var = RequestContext(request, {'page_title': u'Devrank', })
         return render_to_response('home.html', var)
 
-def social(request):
-    var = RequestContext(request, {
-            'page_title': u'social graph',
-            })
-    return render_to_response('social.html', var)
+class search(View):
+    def get(self, request, *args, **kwargs):
+        if 'search' in kwargs:
+            c = ESClient()
+            r = c.search(kwargs['search'])
+
+            for _r in r:
+                _r.update({u"source":_r["_source"]})
+                if u"devrank_score" in _r[u"source"].keys():
+                    value = float(_r[u"source"][u"devrank_score"]) * 1000000
+                    _r["source"].update({u"devrank_score":value})
+                else:
+                    _r["source"].update({u"devrank_score":0})
+                _r.pop(u"_source",None)
+
+            var = RequestContext(request, {
+                    'page_title': u'Devrank',
+                    'results': r,
+                    'query':kwargs['search'],
+                    })
+            return render_to_response('result_list.html', var)
+
 
 def social_json(request):
     c = ESClient()
--- www/icecream/urls.py
+++ www/icecream/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from icecream.bibibig.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,8 +7,9 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'icecream.bibibig.views.home', name='home'),
-    url(r'^social/$', 'icecream.bibibig.views.social', name='social'),
+    url(r'^$', home.as_view(), name='home'),
+    url(r'^search/(?P<search>\w+)/?$', search.as_view()),
+    # url(r'^social/$', 'icecream.bibibig.views.social', name='social'),
     url(r'^social.json/$', 'icecream.bibibig.views.social_json', name='social_json'),
     # url(r'^icecream/', include('icecream.foo.urls')),
 
