925d7632f2f9560aaadd3a4f1fb8536d5fccb3a3
Jongyeol Choi <jong10@gmail.com>
2013-09-11 01:22:29 +0900

    www: Implement search, get_gravatar_url

--- www/icecream/bibibig/devrankmodels.py
+++ www/icecream/bibibig/devrankmodels.py
@@ -3,6 +3,7 @@ import icecream.settings
 import sys
 sys.path.append('../devrank-sqlclient')
 
+from sqlalchemy import or_
 from models import *
 from client import *
 
@@ -11,11 +12,23 @@ class DevRankModel(object):
     def __init__(self):
         self.db = DevRankDB(settings.SQLALCHEMY)
         self.db.connect()
-        pass
+
     def get_gravatar_url(self, user):
-        pass
+        s = self.db.makesession()
+        u = s.query(User).filter(User.login==user).first()
+        return u and u.avatar_url or None
+
     def search(self, query):
         s = self.db.makesession()
-        return s.query(User).all()
+        likequery = '%%%s%%' % query
+        q = s.query(User).join(Repo, User.id == Repo.owner_id) \
+             .filter(or_(Repo.description.like(likequery), \
+                         Repo.language.like(likequery))) \
+             .order_by(User.devrank_score.desc())
+             .limit(20)
+             .all()
+        return q
+
     def social_search(self, users):
+        # TODO
         pass
