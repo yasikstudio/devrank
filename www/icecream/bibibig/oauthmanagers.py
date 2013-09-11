f7d12a28ff92a1bf3cbd05636d597a68ddea3283
rakgyu.song <reeoss@gmail.com>
2013-09-12 00:02:13 +0900

    www:modify import module

--- www/icecream/bibibig/oauthmanagers.py
+++ www/icecream/bibibig/oauthmanagers.py
@@ -1,6 +1,7 @@
-import icecream.settings as settings
 import os
 from requests_oauthlib import OAuth2Session
+from icecream.settings import GITHUB_APP_ID
+from icecream.settings import GITHUB_API_SECRET
 
 os.environ["DEBUG"]="true"
 github_authorization_base_url = 'https://github.com/login/oauth/authorize'
@@ -8,7 +9,7 @@ github_token_url = 'https://github.com/login/oauth/access_token'
 
 class OAuthManager(object):
     def __init__(self):
-        self.github = OAuth2Session(settings.GITHUB_APP_ID)
+        self.github = OAuth2Session(GITHUB_APP_ID)
 
     def login_request(self):
         authorization_url, status = \
@@ -17,7 +18,7 @@ class OAuthManager(object):
 
     def callback_response(self, url):
         self.github.fetch_token(github_token_url,
-                        client_secret = settings.GITHUB_API_SECRET,
+                        client_secret = GITHUB_API_SECRET,
                         authorization_response = url)
     def getUser(self):
         r = self.github.get('https://api.github.com/user')
